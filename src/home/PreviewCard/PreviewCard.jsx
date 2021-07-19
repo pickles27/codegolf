f371118ba8d16cb4e69e59b7a47d35fffe428d9d
pickles27 <spickrahn@gmail.com>
2021-07-18 19:39:40 -0700

    remove styled components and simplify a wee bit

--- src/home/PreviewCard/PreviewCard.jsx
+++ src/home/PreviewCard/PreviewCard.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import './PreviewCard.css';
-import { Button } from '../../components/Button'
 
 export const PreviewCard = ({
     author,
     prompt,
     title,
-}) => {
-
-const handleChallenge = () => {
-    //go to challenge submission page
-}
-
-return <div className="card">
+}) => <div className="card">
     <h1>{title}</h1>
     <h4>{author}</h4>
     <p>{prompt}</p>
-    <Button onClick={handleChallenge}>Challenge</Button>
-</div>};
-
+    <button>Challenge</button>
+</div>;
