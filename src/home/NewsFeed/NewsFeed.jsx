168236f54bd97e5d4fad3c1d3eba165ede7d8734
pickles27 <spickrahn@gmail.com>
2021-07-25 15:27:53 -0700

    add styling to NewsFeed component

--- src/home/NewsFeed/NewsFeed.jsx
+++ src/home/NewsFeed/NewsFeed.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { PreviewCard } from '../PreviewCard'
+import './NewsFeed.css';
 
 export const NewsFeed = ({submissions}) => (
-  <div>
+  <div className="feed">
     {submissions.map(submission => <PreviewCard key={submission._id} {...submission} />)}
   </div>
 )
--- src/mockData.js
+++ src/mockData.js
@@ -8,13 +8,13 @@ export const mockSubmissions = [
   {
     title: 'Chiliiii!!!1',
     author: 'Serrerrrr',
-    prompt: `How many Darts does it take to get Kevin's attention`,
+    prompt: `How many Darts does it take to get Kevin's attention? This should be multiline without truncating.`,
     _id: '2',
   },
   {
     title: 'Kleenex thief',
     author: 'Maddie',
-    prompt: 'How many tissues can Maddie steal in 1 hour',
+    prompt: 'How many tissues can Maddie steal in 1 hour? I think we may know the answer soon.',
     _id: '3',
   },
   {
