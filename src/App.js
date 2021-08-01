c94657124a38b0e3a88b80b980fc5b76d729fcc7
pickles27 <spickrahn@gmail.com>
2021-08-01 14:04:11 -0700

    move submissions to be fetched inside NewsFeed component in future

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { NewsFeed } from './home/NewsFeed/NewsFeed';
-import { mockSubmissions } from './mockData';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NewsFeed submissions={mockSubmissions} />
+      <NewsFeed />
     </div>
   );
 }
