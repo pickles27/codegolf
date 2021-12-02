47fc669cbf7e1a81a83b6a221fd1e209a0842e51
Sarah Pickrahn <spickrahn@gmail.com>
2021-12-01 21:27:40 -0800

    prettify

--- src/navbar/NavBar/NavBar.jsx
+++ src/navbar/NavBar/NavBar.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import BlurOnOutlinedIcon from '@mui/icons-material/BlurOnOutlined';
-import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@mui/material/Button";
+import BlurOnOutlinedIcon from "@mui/icons-material/BlurOnOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 
 export const NavBar = () => {
-    const { push } = useHistory();
+  const { push } = useHistory();
 
-  return <AppBar color="inherit" position="sticky" >
-    <Toolbar sx={{justifyContent: 'space-between'}}>
-      <Button color="inherit" startIcon={<BlurOnOutlinedIcon/>} onClick={() => push('/')}>make my code smaller</Button>
-      <Button color="inherit" startIcon={<AddCircleOutlineOutlinedIcon/>} onClick={() => push('/submit')}>Submit</Button>
-      <Button color="inherit">Profile</Button>
-    </Toolbar>
-  </AppBar>
-}
-
+  return (
+    <AppBar color="inherit" position="sticky">
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Button
+          color="inherit"
+          startIcon={<BlurOnOutlinedIcon />}
+          onClick={() => push("/")}
+        >
+          make my code smaller
+        </Button>
+        <Button
+          color="inherit"
+          startIcon={<AddCircleOutlineOutlinedIcon />}
+          onClick={() => push("/submit")}
+        >
+          Submit
+        </Button>
+        <Button color="inherit">Profile</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
